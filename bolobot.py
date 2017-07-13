f547c56eb7567c48ca4a9548d7cc8522a235c7cf
Felipe Sousa <felipe.sousa.f.s.s@gmail.com>
2017-07-14 01:18:46 +0200

    Upgrading to Slack latest API

--- bolobot.py
+++ bolobot.py
@@ -4,10 +4,8 @@ import json
 import requests
 
 from bs4 import BeautifulSoup, SoupStrainer
-from slackclient import SlackClient
 from yahoo_finance import Currency
 from selenium import webdriver
-from timeout import timeout
 
 
 class BoloBot():
@@ -16,8 +14,7 @@ class BoloBot():
     INFOMONEY_URL = 'http://www.infomoney.com.br/mercados/cambio'
     BB_PERCENTAGE = 0.9862
 
-    def __init__(self, token):
-        self._sc = SlackClient(token)
+    def __init__(self):
         self._driver = webdriver.PhantomJS()
         self._currency_types = {'!dtc2': self._get_tourist_exchange_rate,
                               '!dollar': self._get_dollar_exchange_rate,
@@ -31,7 +28,7 @@ class BoloBot():
         soup = BeautifulSoup(response.text, 'lxml', parse_only=table_data)
         for td in soup:
             if td.string is not None and td.string.strip() == u'Dólar Turismo':
-                return td.next_sibling.string.strip().replace(',', '.')
+                return td.next_sibling.next_sibling.string.strip().replace(',', '.')
 
     def _get_tourist_exchange_rate(self):
         self._driver.get(self.UOL_URL)
@@ -51,24 +48,17 @@ class BoloBot():
     def _get_euro_exchange_rate(self):
         eur_brl = Currency('EURBRL')
         return eur_brl.get_bid()
+    
+    def _send_rate(self, channel, rate_func, currency='USD', quantity=1):
+        try:
+            rate = float(rate_func())
+            total = '%.4f' % (rate * quantity)
+            message = '%s %s = %s BRL' % (str(quantity), currency, total)
+            return message
+        except:
+            return "Update nedded. Source has changed."
 
-    def _get_user(self, id):
-        member = json.loads(self._sc.api_call('users.info', user=id))['user']
-        return member['name']
-
-    def _send_message(self, user, channel, text):
-        self._sc.api_call('chat.postMessage', as_user='true:',
-                          channel=channel, text=text)
-
-    def _send_rate(self, user, channel, rate_func, currency='USD', quantity=1):
-        user_name = self._get_user(user)
-        rate = float(rate_func())
-        total = '%.4f' % (rate * quantity)
-        message = '<@%s>: %s %s = %s BRL' % (user_name, str(quantity), currency, total)
-        self._send_message(user, channel, message)
-
-    @timeout(25)
-    def _reply_message(self, message, user_id, channel):
+    def _reply_message(self, message, channel):
         message_list = message.split()
         command = message_list[0]
         quantity = 1
@@ -78,26 +68,7 @@ class BoloBot():
             except ValueError as e:
                 pass
         if command == '!euro':
-            self._send_rate(user_id, channel, self._get_euro_exchange_rate, 'EUR', quantity)
+            return self._send_rate(channel, self._get_euro_exchange_rate, 'EUR', quantity)
         elif command in self._currency_types.keys():
             func = self._currency_types.get(command)
-            self._send_rate(user_id, channel, func, quantity=quantity)
-
-    def run(self):
-        if not self._sc.rtm_connect():
-            exit('Failed to connect.')
-        while True:
-            new_events = self._sc.rtm_read()
-            for evt in new_events:
-                print(evt)
-                if 'type' in evt:
-                    if evt['type'] == 'message' and 'text' in evt:
-                        channel = evt['channel']
-                        message = evt['text']
-                        uid = evt['user']
-                        user_id = str(uid)
-                        try:
-                            self._reply_message(message, user_id, channel)
-                        except Exception as e:
-                            print str(e)
-            #time.sleep(1)
+            return self._send_rate(channel, func, quantity=quantity)
--- main.py
+++ main.py
@@ -1,11 +1,48 @@
 import os
-import sys
+import time
 from dotenv import load_dotenv
 from bolobot import BoloBot
+from slackclient import SlackClient
 
 DOTENV_PATH = os.path.join(os.path.dirname(__file__), '.env')
-
-
 load_dotenv(DOTENV_PATH)
-bot = BoloBot(os.environ.get('BOT_TOKEN'))
-bot.run()
+
+
+BOT_ID = os.environ.get('BOT_ID') #bolobot
+AT_BOT = "<@" + BOT_ID + ">"
+
+slack_client = SlackClient(os.environ.get('BOT_TOKEN'))
+
+
+
+def reply_command(channel, response):
+    slack_client.api_call("chat.postMessage", channel=channel,
+                          text=response, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel']
+    return None, None
+
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if slack_client.rtm_connect():
+        print("StarterBot connected and running!")
+        bot = BoloBot()
+        while True:
+            command, channel = parse_slack_output(slack_client.rtm_read())
+            if command and channel:
+                response = bot._reply_message(command, channel)
+                print("RESPONSE %s", response)
+                reply_command(channel, response)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
